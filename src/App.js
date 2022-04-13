a08e34c402d559b139c2c12cea2b626f52841858
themostluckytina <themostluckytina@gmail.com>
2022-04-13 17:30:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [picture, setPicture] = useState(0);
+
+  const {id, name, description, image, price} = data[picture];
+  
+
+const backStep =()=>{
+  setPicture((picture =>{
+    picture --;
+    if(picture < 0) {
+      picture = data.length-1;
+    }
+    return picture
+  }))
+}
+
+
+ const nextStep =()=>{
+   setPicture ((picture=>{
+     picture++;
+     if( picture > data.length-1){
+       picture= 0;
+     }
+     return picture
+   }))
+ }
+  
+  
+
+return( <div className='App '>
+
+  <div className='top'>
+    <p className='auction'>АУКЦИОННЫЙ ДОМ</p>
+    <h1>WorldArt</h1>
+  </div>
+
+  <div className='top'>
+  <h2>АНОНСИРУЕТ НОВЫЕ ЛОТЫ </h2>
+  <h3>Тема: "Женские образы в живописи"</h3>
+</div>
+
+<div className='item'>
+  <div>
+  <h2 className='lot'> Лот № {id} </h2>
+  <p className='pic'>{description } </p>
+  <p className='pic'>Автор: {name} </p>
+  </div>
+
+  <div>
+    <p className='price'>Стартовая цена: {price} </p>
+  </div>
+
+ 
+<div>
+  <img src={image} width={400} alt='pic' />
+</div>
+
+<div>
+  <button onClick={backStep}> назад </button>
+  <button onClick={nextStep}> вперед </button>
+</div>
+
+<br></br>
+
+</div>
+
+<div className='footer'>
+  <h3 className='mail'>Получайте все события Аукционного Дома</h3>
+</div>
+<div>
+  <input type='text'  placeholder='Напиши свой E-mail' />
+  <button className='send'>Подписаться</button>
+</div>
+
+</div>
+)
+
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+        id: '387.908/168',
+        name: 'Морган Уэйстлинг  (1964 г.р)',
+        description: '"Девушка в лиловом", 1996, акварель 45х73 ',
+        image: 'https://i.pinimg.com/736x/79/0d/bc/790dbcb21b1ed23ff97953767e83e294--art-children-figure-painting.jpg',
+        price: '$10000'
+    },
+    {
+        id: '38790/621',
+        name: 'Ромэйн Брукс (1874-1970)',
+        description: '"Гордость", 1930, акварель 68х83',
+        image: 'https://i.pinimg.com/736x/ef/34/de/ef34de37b4f5ebfdbada703fdb12bd3d--romaine-brooks-muriel.jpg',
+        price:'$30000'
+    },
+    {
+        id: '38790/621',
+        name: 'Альфонс Муха (1860-1939)',
+        description: '"Соловьи", 1892, акварель 45х50',
+        image: 'https://i.etsystatic.com/6307147/r/il/80dcf4/1301036252/il_fullxfull.1301036252_j11i.jpg',
+        price:'$300000'
+    },
+    {
+        id: '38790/621',
+        name: 'Луи Мария де Шривер (1862 - 1942)',
+        description: '"Продавщица цветов", 1897, акварель 91х 73',
+        image: 'https://когданечегоделать.рф/wp-content/uploads/2021/03/5606572.jpg',
+        price:'$100000'
+    },
+    {
+        id: '38790/621',
+        name: 'Рохелио де Игускиз (1845-1915)',
+        description: '"Камилла де Маршенал", 1873, акварель 98х125',
+        image: 'https://i.pinimg.com/736x/36/ae/94/36ae94a2d4bb297d583df425773744ba--love-letters-visual-arts.jpg',
+        price:'$50000'
+    },
+    {
+        id: '38790/621',
+        name: 'Владимир Мухин (1971)',
+        description: '"Два образа", 2009, масло, холст 80х68',
+        image: 'https://mtdata.ru/u2/photoF3C7/20525401529-0/original.jpg',
+        price:'$5000'
+    },
+    {
+        id: '38790/621',
+        name: 'Владимир Мухин (1971)',
+        description: '"Чаепитие", 2007, акварель 68х89',
+        image: 'https://ic.pics.livejournal.com/tanjand/44781189/3799561/3799561_original.jpg',
+        price:'$3000'
+    },
+    {
+        id: '38790/621',
+        name: 'Брент Хейтон (1954)',
+        description: '"Париж", 1992, акварель 58х69 ',
+        image: 'https://i.pinimg.com/originals/24/04/7f/24047f7552123a8a7a5900b471c9b2f5.jpg',
+        price:'$8000'
+    },
+    {
+        id: '38790/621',
+        name: 'Андерс Цорн(1860-1920)',
+        description: '"Летнее развлечение", 1900, акварель 78х58',
+        image: 'https://3.bp.blogspot.com/-iBUhxLnFQ4M/We6M4IJ3nqI/AAAAAAAAIAQ/GWA0DEALc14acFpqi-SHW77velqnjd_IQCLcBGAs/s1600/Zorn-Aquarelle-Buisson-1886.jpg',
+        price:'$15000'
+    }
+]
